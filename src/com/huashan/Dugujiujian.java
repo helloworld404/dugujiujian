0c0f3727573bb23f58d39beafd8af1a145fbe375
岳不群 <helloworld369@qq.com>
2017-02-28 13:01:40 +0800

    update 02

--- src/com/huashan/Dugujiujian.java
+++ src/com/huashan/Dugujiujian.java
@@ -4,6 +4,8 @@ public class Dugujiujian {
 	
 	public static void main(String[] args) {
 		System.out.println("hello dugujiujian ");
+		System.out.println("hello dugujiujian1 ");
+		System.out.println("hello dugujiujian2 ");
 		
 	}
 
